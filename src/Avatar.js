3503db848499e38bdc72cb91f1a2b3cd5862d213
chris <chrishethertonweb@gmail.com>
2022-10-17 19:55:25 +0200

    added props classes to Avatar.js

--- src/Avatar.js
+++ src/Avatar.js
@@ -1,10 +1,21 @@
-function Avatar() {
-  return (
-    <img src="https://picsum.photos/400/400" alt="Avatar" />
-    // `<img className='${size}' src="https://picsum.photos/400/400" alt="Avatar" />`
+function Avatar({ size, shape }) {
 
-    // size = "xl"
-    // type = "{circle}" 
+
+  return (
+    <div>
+      <img src="https://picsum.photos/400/400" alt="Avatar" />
+      <select className="w-[100px]" placeholder="Size" name="size" id="">
+        <option value={size.s}>S</option>
+        <option value={size.m}>M</option>
+        <option value={size.l}>L</option>
+        <option value={size.xl}>XL</option>
+      </select>
+      <select className="w-[100px]" placeholder="Shape" name="shape" id="">
+        <option value={shape.square}>Square</option>
+        <option value={shape.round}>Rounded</option>
+        <option value={shape.circle}>Circle</option>
+      </select>
+    </div>
   );
 }
 
